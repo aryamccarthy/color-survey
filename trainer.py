ae6fccc8eff22d2af059e6576e6a4a8e75c4cdfa
Arya McCarthy <arya.mccarthy@gmail.com>
2018-06-02 17:07:02 -0400

    flake8 the trainer

--- trainer.py
+++ trainer.py
@@ -41,7 +41,7 @@ class PyTorchTrainer(object):
                 now = time.time()
                 total_time = (now - start) / done
                 tqdm.write(f'\r\ttrained for {iteration} iterations, loss {running_loss / iteration} '
-                                 f'{int(done*100)}% time: {int((now - start) / 60)}/{int(total_time / 60)} (min)  ') 
+                           f'{int(done*100)}% time: {int((now - start) / 60)}/{int(total_time / 60)} (min)  ') 
             if self._evaluate:
                 tqdm.write('\n')
                 self._evaluate(self._model)
